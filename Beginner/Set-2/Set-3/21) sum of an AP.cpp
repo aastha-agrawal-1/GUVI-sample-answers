2db341e787e1705bd33516e525f6877412c0261f
Aastha Agrawal <aastha.agrawal_cs16@gla.ac.in>
2019-07-03 23:14:01 +0530

    Add files via upload

---
+++ Beginner/Set-2/Set-3/21) sum of an AP.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n,a,d;
+	cin>>n>>a>>d;
+	double sum;
+	sum=(n*0.5)*((2*a)+(n-1)*d);
+	cout<<sum;
+}
---
+++ Beginner/Set-2/Set-3/22)print largest of n elements.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	long int array[n];
+	for(int i=0;i<n;i++){
+		cin>>array[i];
+	}
+	sort(array,array+n);
+	cout<<array[n-1];
+}
